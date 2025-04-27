57214c3d067450276d0b811ff3a08dcabc9036b3
novanz24 <novanzaedgarwibowo@gmail.com>
2025-04-27 21:20:22 +0700

    Update Manajer.java

--- 24060123130055/Pertemuan 7/9a/Manajer.java
+++ 24060123130055/Pertemuan 7/9a/Manajer.java
@@ -1,4 +1,10 @@
-// Subclass Manajer
+/*
+ File         : Manajer.java
+ Deskripsi    : Subclass Pegawai yang merepresentasikan manajer, menambahkan atribut tunjangan.
+ Pembuat      : Novanza Edgar Wibowo / 24060123130055
+ Tanggal      : 27 April 2025
+*/
+
 public class Manajer extends Pegawai {
     private int tunjangan = 700_000;
 
