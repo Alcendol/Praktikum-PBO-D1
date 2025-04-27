736301ee9abcb14f3b52b2e3f2824ebb21bfba29
novanz24 <novanzaedgarwibowo@gmail.com>
2025-04-27 21:19:40 +0700

    Update Burung.java

--- 24060123130055/Pertemuan 7/9b/Burung.java
+++ 24060123130055/Pertemuan 7/9b/Burung.java
@@ -1,3 +1,10 @@
+/*
+ File         : Burung.java
+ Deskripsi    : Subclass Anabul yang mewakili burung, dengan implementasi Gerak dan Bersuara yang spesifik.
+ Pembuat      : Novanza Edgar Wibowo / 24060123130055
+ Tanggal      : 27 April 2025
+*/
+
 public class Burung extends Anabul {
     public Burung(String nama) {
         super(nama);
